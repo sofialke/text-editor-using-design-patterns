10aa30aafe99c498157a6d49514c63dbb254ecec
Zosia Walczewska <zosia.walczewska@gmail.com>
2021-10-19 15:10:59 +0200

    Initialisation of skeleton of selection implementation class.

--- src/TextEditor/SelectionImpl.java
+++ src/TextEditor/SelectionImpl.java
@@ -1,4 +1,70 @@
 package TextEditor;
 
-public class SelectionImpl {
+public class SelectionImpl implements Selection{
+
+    /**
+     * Provides the index of the first character designated
+     * by the selection.
+     *
+     * @return
+     */
+    @Override
+    public int getBeginIndex(){
+
+    }
+
+    /**
+     * Provides the index of the first character
+     * after the last character designated
+     * by the selection.
+     *
+     * @return the end index
+     */
+    @Override
+    public int getEndIndex(){
+
+    }
+
+    /**
+     * Provides the index of the first character in the buffer
+     *
+     * @return the buffer's begin index
+     */
+    @Override
+    public int getBufferBeginIndex(){
+
+    }
+
+    /**
+     * Provides the index of the first "virtual" character
+     * after the end of the buffer
+     *
+     * @return the post end buffer index
+     */
+    @Override
+    public int getBufferEndIndex(){
+
+    }
+
+    /**
+     * Changes the value of the begin index of the selection
+     *
+     * @param beginIndex, must be within the buffer index range
+     * @throws IndexOutOfBoundsException if the beginIndex is out of bounds
+     */
+    @Override
+    public void setBeginIndex(int beginIndex){
+
+    }
+
+    /**
+     * Changes the value of the end index of the selection
+     *
+     * @param endIndex, must be within the buffer index range
+     * @throws IndexOutOfBoundsException if the beginIndex is out of bounds
+     */
+    @Override
+    public void setEndIndex(int endIndex){
+
+    }
 }
