50a8abc47189706378f8320cb4c2c3eae07eec5d
Zosia Walczewska <zosia.walczewska@gmail.com>
2021-10-22 16:26:05 +0200

    first 5 test methods

--- src/TextEditor/EngineTest.java
+++ src/TextEditor/EngineTest.java
@@ -2,12 +2,17 @@ package TextEditor;
 
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.Tag;
+import static org.junit.jupiter.api.Assertions.assertThrows;
 
 import static org.junit.jupiter.api.Assertions.*;
 
 class EngineTest {
 
     private Engine engine;
+    private static String TEST_STRING = "This is a test string that will be asserted";
+    private static String EXCEPTION_MESSAGE_WRONG_BEGIN_INDEX = "Begin index can't be bigger than end index";
+    private static String EXCEPTION_MESSAGE_WRONG_END_INDEX = "End index can't be smaller than end index";
 
     @org.junit.jupiter.api.BeforeEach
     void setUp() {
@@ -27,26 +32,42 @@ class EngineTest {
 
     @Test
     void getBufferContents() {
-        todo();
+        engine.insert(TEST_STRING);
+        assertEquals(TEST_STRING, engine.getBufferContents());
+        engine.delete();
+        assertEquals("", engine.getBufferContents());
     }
 
     @Test
     void getClipboardContents() {
-        todo();
+        engine.insert(TEST_STRING);
+        Selection selection = engine.getSelection();
+        selection.setBeginIndex(0);
+        selection.setEndIndex(TEST_STRING.length()-1);
+        engine.copySelectedText();
+        assertEquals(TEST_STRING, engine.getClipboardContents());
     }
 
     @Test
     void cutSelectedText() {
-        todo();
+        engine.insert(TEST_STRING);
+        Selection selection = engine.getSelection();
+        selection.setBeginIndex(0);
+        selection.setEndIndex(TEST_STRING.length()-1);
+        engine.cutSelectedText();
+        assertEquals(TEST_STRING, engine.getClipboardContents());
+        assertEquals("", engine.getBufferContents());
     }
 
     @Test
     void copySelectedText() {
-        todo();
+        engine.insert(TEST_STRING);
+        Selection selection = engine.getSelection();
+        selection.setBeginIndex(0);
+        selection.setEndIndex(TEST_STRING.length()-1);
+        engine.copySelectedText();
+        assertEquals(TEST_STRING, engine.getClipboardContents());
+        assertEquals(TEST_STRING, engine.getBufferContents());
     }
 
-    @Test
-    void pasteClipboard() {
-        todo();
-    }
 }
