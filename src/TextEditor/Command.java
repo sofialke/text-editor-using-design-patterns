893201d5dcd8e29b1585dee6b629ca289e6c1e75
Zosia Walczewska <zosia.walczewska@gmail.com>
2021-11-09 11:23:14 +0100

    deleting command interface, it will be added in V2

--- src/TextEditor/Command.java
+++
@@ -1,8 +0,0 @@
-package TextEditor;
-
-public interface Command {
-    /**
-     * Execute method which will be invoked by the invoker who doesn't know what kind of command will be executed.
-     */
-    void execute();
-}
