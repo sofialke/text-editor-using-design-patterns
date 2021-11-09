415a678d8947f8e2faf59e83c28b8034e74b463b
Zosia Walczewska <zosia.walczewska@gmail.com>
2021-11-09 12:16:18 +0100

    tests for commands classes added - coverage 99%

--- src/TextEditor/CommandsTest.java
+++ src/TextEditor/CommandsTest.java
@@ -5,5 +5,90 @@ import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.Tag;
 import static org.junit.jupiter.api.Assertions.*;
 
-public class CommandsTest {
+class CommandsTest {
+    private Engine engine;
+    private Command command;
+    private static String TEST_STRING = "This is a test string that will be asserted";
+
+    @org.junit.jupiter.api.BeforeEach
+    void setUp() {
+        engine = new EngineImpl();
+    }
+
+    @Test
+    @DisplayName("Text must be inserted using InsertCommand")
+    void insertTextUsingInsertCommandClass() {
+        command = new InsertCommand(engine, TEST_STRING);
+        command.execute();
+        assertEquals(TEST_STRING,engine.getBufferContents());
+    }
+
+    @Test
+    @DisplayName("Empty text must be copied using CopyCommand")
+    void copyEmptyTextUsingCopyCommandClassWhenNoTextWasInserted() {
+        command = new CopyCommand(engine);
+        command.execute();
+        assertEquals("",engine.getClipboardContents());
+    }
+
+    @Test
+    @DisplayName("Not-empty text must be copied using CopyCommand")
+    void copyTextUsingCopyCommandClassWhenTextWasInsertedAndSelected() {
+        command = new InsertCommand(engine, TEST_STRING);
+        command.execute();
+        Selection selection = engine.getSelection();
+        selection.setEndIndex(TEST_STRING.length());
+        command = new CopyCommand(engine);
+        command.execute();
+        assertEquals(TEST_STRING,engine.getClipboardContents());
+    }
+
+    @Test
+    @DisplayName("Text must be pasted using PasteCommand")
+    void pasteTextUsingPasteCommandClassWhenTextWasInsertedSelectedAndCopied() {
+        command = new InsertCommand(engine, TEST_STRING);
+        command.execute();
+        Selection selection = engine.getSelection();
+        selection.setEndIndex(TEST_STRING.length());
+        command = new CopyCommand(engine);
+        command.execute();
+        selection.setEndIndex(0);
+        command = new PasteCommand(engine);
+        command.execute();
+        assertEquals(TEST_STRING+TEST_STRING,engine.getBufferContents());
+    }
+
+    @Test
+    @DisplayName("Empty text must be pasted using PasteCommand")
+    void pasteEmptyTextUsingPasteCommandClassWhenCopiedTextWasEmpty() {
+        command = new InsertCommand(engine, TEST_STRING);
+        command.execute();
+        command = new CopyCommand(engine);
+        command.execute();
+        command = new PasteCommand(engine);
+        command.execute();
+        assertEquals(TEST_STRING,engine.getBufferContents());
+    }
+
+    @Test
+    @DisplayName("Text must be cut using CutCommand")
+    void cutEmptyTextUsingCutCommandClassWhenSelectedTextWasEmpty() {
+        command = new InsertCommand(engine, TEST_STRING);
+        command.execute();
+        command = new CutCommand(engine);
+        command.execute();
+        assertEquals(TEST_STRING,engine.getBufferContents());
+    }
+
+    @Test
+    @DisplayName("Text must be cut using CutCommand")
+    void cutAllTextUsingCutCommandClassWhenTheWholeTextWasSelected() {
+        command = new InsertCommand(engine, TEST_STRING);
+        command.execute();
+        Selection selection = engine.getSelection();
+        selection.setEndIndex(TEST_STRING.length());
+        command = new CutCommand(engine);
+        command.execute();
+        assertEquals("",engine.getBufferContents());
+    }
 }
