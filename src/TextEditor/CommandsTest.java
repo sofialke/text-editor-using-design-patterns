516fa4b7ab390f02d62a50d6e3d32ed0012c0867
Zosia Walczewska <zosia.walczewska@gmail.com>
2021-11-09 11:27:52 +0100

    initialisation of test class for Command classes

---
+++ src/TextEditor/CommandsTest.java
@@ -0,0 +1,9 @@
+package TextEditor;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.Tag;
+import static org.junit.jupiter.api.Assertions.*;
+
+public class CommandsTest {
+}
