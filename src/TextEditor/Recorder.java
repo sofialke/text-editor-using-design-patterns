0fbffd3a36191f5748b7707e1ca1a39fe310dc0e
Zosia Walczewska <zosia.walczewska@gmail.com>
2021-11-15 16:41:23 +0100

    preconditions added for recorder methods - input params cannot be null

--- src/TextEditor/Recorder.java
+++ src/TextEditor/Recorder.java
@@ -1,15 +1,22 @@
-package TextEditor;public class Recorder {
+package TextEditor;
+
+import java.util.Objects;
+
+public class Recorder {
 		
 	private InsertMemento memento;
 	private InsertCommand command;
 	
 	public Recorder(InsertCommand command, InsertMemento memento) {
+		Objects.requireNonNull(command);
+		Objects.requireNonNull(memento);
 		this.command = command;
 		this.memento = memento;
 	}
 	
-	public void save(InsertCommand c) {
-		this.memento = c.getMemento(); 	
+	public void save(InsertCommand command) {
+		Objects.requireNonNull(command);
+		this.memento = c.getMemento();
 	}
 
 	
