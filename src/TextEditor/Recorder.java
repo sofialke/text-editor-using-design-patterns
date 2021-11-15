e0cf342f669b1f22a921afc96eae992e310211a8
Zosia Walczewska <zosia.walczewska@gmail.com>
2021-11-15 16:44:14 +0100

    comments added to recorder methods

--- src/TextEditor/Recorder.java
+++ src/TextEditor/Recorder.java
@@ -6,17 +6,28 @@ public class Recorder {
 		
 	private InsertMemento memento;
 	private InsertCommand command;
-	
+
+	/**
+	 * Recorder initializer with parameters.
+	 * Preconditions - parameters cannot be null.
+	 * @param command
+	 * @param memento
+	 */
 	public Recorder(InsertCommand command, InsertMemento memento) {
 		Objects.requireNonNull(command);
 		Objects.requireNonNull(memento);
 		this.command = command;
 		this.memento = memento;
 	}
-	
+
+	/**
+	 * A method to save a command.
+	 * Precondition - input parameter cannot be null.
+	 * @param command
+	 */
 	public void save(InsertCommand command) {
 		Objects.requireNonNull(command);
-		this.memento = c.getMemento();
+		this.memento = command.getMemento();
 	}
 
 	
