9e0187b636b96e290a74d9b3bab64469daafa900
Marta <marta.moreno@etudiant.univ-rennes1.fr>
2021-11-18 17:31:45 +0100

    recorder added to insert command

--- src/TextEditor/InsertCommand.java
+++ src/TextEditor/InsertCommand.java
@@ -5,16 +5,19 @@ package TextEditor;
 
 public class InsertCommand implements Command{
     Engine engine;
+    private Recorder recorder;
 	private String textToBeInserted;
     private Boolean wasReplayed = false;
 
-    public InsertCommand(Engine engine, Invoker invoker){
+    public InsertCommand(Engine engine, Invoker invoker, Recorder recorder){
         this.engine = engine;
+        this.recorder = recorder;
         this.textToBeInserted = invoker.getTextToBeInserted();
     }
 
     public void execute(){
         engine.insert(this.textToBeInserted);
+        recorder.save(this);
         this.wasReplayed = false;
     }
     
