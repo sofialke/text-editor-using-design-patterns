eed521e7aa61d70b52bfcfde5b41b421d7d7f280
Zosia Walczewska <zosia.walczewska@gmail.com>
2021-11-18 16:46:44 +0100

    Invoker implementation class

---
+++ src/TextEditor/InvokerImpl.java
@@ -0,0 +1,61 @@
+package TextEditor;
+
+import org.junit.jupiter.params.shadow.com.univocity.parsers.annotations.NullString;
+
+import java.util.Collection;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Set;
+
+public class InvokerImpl implements Invoker{
+    //map with an id for every command, only one instance per command. there will be a parameter that will hold the text
+    //that will be inserted. the map will have keys- I for Insert, D for Delete itp
+
+    private Engine engine;
+    private String textToBeInserted;
+    private Map<String,Command> mapOfCOmmands;
+    private Integer beginIndex = 0;
+    private Integer endIndex = 0;
+
+    public InvokerImpl(Engine engineInput){
+        engine = engineInput;
+        mapOfCOmmands = new HashMap<>();
+        mapOfCOmmands.put("I", new InsertCommand(engine, this));
+        mapOfCOmmands.put("CO", new CopyCommand(engine));
+        mapOfCOmmands.put("CU", new CutCommand(engine));
+        mapOfCOmmands.put("S", new SelectionCommand(engine, this));
+        mapOfCOmmands.put("D", new CutCommand(engine));
+        mapOfCOmmands.put("P", new PasteCommand(engine));
+    }
+
+    public String getTextToBeInserted(){
+        return this.textToBeInserted;
+    }
+
+    public Integer getBeginIndex(){
+        return this.beginIndex;
+    }
+
+    public Integer getEndIndex(){
+        return this.endIndex;
+    }
+
+    public void setTextToBeInserted(String text){
+        this.textToBeInserted = text;
+        mapOfCOmmands.put("I", new InsertCommand(engine, this));
+    }
+
+    public void setBeginIndex(Integer index){
+        this.beginIndex = index;
+        mapOfCOmmands.put("S", new SelectionCommand(engine, this));
+    }
+
+    public void setEndIndex(Integer index){
+        this.endIndex = index;
+        mapOfCOmmands.put("S", new SelectionCommand(engine, this));
+    }
+
+    public void execute(String keyName){
+        mapOfCOmmands.get(keyName).execute();
+    }
+}
