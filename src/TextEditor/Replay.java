a7199c5b67342a8df3f4fbe9778331cd91f5d344
Zosia Walczewska <zosia.walczewska@gmail.com>
2021-11-23 20:57:19 +0100

    replay command added

--- src/TextEditor/Replay.java
+++ src/TextEditor/Replay.java
@@ -1,5 +1,23 @@
 package TextEditor;
+import java.util.Optional;
 
-public class Replay {
+public class Replay implements Recordable{
+    Engine engine;
+    Recorder recorder;
+    public Replay(Engine engine, Invoker invoker){
+        this.engine = engine;
+        this.recorder = invoker.getRecorder();
+    }
 
+    public Optional<Memento> getMemento(){
+        return Optional.empty();
+    }
+
+    public void setMemento(Memento memento){
+
+    }
+
+    public void execute(){
+        recorder.replay();
+    }
 }
