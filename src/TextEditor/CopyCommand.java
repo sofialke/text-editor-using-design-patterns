94a45936cca32512548c9d513bdc1e1f6d6a19a5
Zosia Walczewska <zosia.walczewska@gmail.com>
2021-11-23 12:10:03 +0100

    copy command fixed

--- src/TextEditor/CopyCommand.java
+++ src/TextEditor/CopyCommand.java
@@ -1,17 +1,32 @@
 package TextEditor;
+
+import java.util.Optional;
+
 /**
  * Class that implements copy command by implementing the Command interface.
  */
 
-public class CopyCommand implements Command{
+public class CopyCommand implements Recordable{
     Engine engine;
+    Recorder recorder;
 
     public CopyCommand(Engine engine){
         this.engine = engine;
+        Optional<Memento> memento = Optional.empty();
+        this.recorder = new Recorder(this);
+    }
+
+    public Optional<Memento> getMemento(){
+        return Optional.empty();
+    }
+
+    public void setMemento(Memento memento){
+
     }
 
     public void execute(){
         engine.copySelectedText();
+        recorder.save(this);
     }
 
 }
