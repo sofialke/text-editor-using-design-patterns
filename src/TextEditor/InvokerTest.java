a6728eb8ddb171eaf8b349d7de4924da17da9aeb
Marta <marta.moreno@etudiant.univ-rennes1.fr>
2021-11-26 18:07:13 +0100

    all commands tested

--- src/TextEditor/InvokerTest.java
+++ src/TextEditor/InvokerTest.java
@@ -3,7 +3,6 @@ package TextEditor;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.Tag;
-import static org.junit.jupiter.api.Assertions.assertThrows;
 
 import static org.junit.jupiter.api.Assertions.*;
 public class InvokerTest {
@@ -52,4 +51,139 @@ public class InvokerTest {
         invoker.execute("I");
         assertEquals(TEST_STRING,engine.getBufferContents());
     }
+    
+    @Test
+    void testExecutingSelectionCommand(){
+        invoker.setTextToBeInserted(TEST_STRING);
+        invoker.addCommand("I", new InsertCommand(engine, this.invoker, this.recorder));
+        invoker.execute("I");
+        invoker.setBeginIndex(3);
+        invoker.setEndIndex(6);
+        invoker.addCommand("S", new SelectionCommand(engine, this.invoker, this.recorder));
+        invoker.execute("S");
+        Selection selectionTest = engine.getSelection();
+        assertEquals(3,selectionTest.getBeginIndex());
+        assertEquals(6,selectionTest.getEndIndex());
+    }
+    
+    
+    @Test
+    void testExecutingCopyCommand() {
+        invoker.setTextToBeInserted(TEST_STRING);
+        invoker.addCommand("I", new InsertCommand(engine, this.invoker, this.recorder));
+        invoker.execute("I");
+        invoker.setBeginIndex(0);
+        invoker.setEndIndex(4);
+        invoker.addCommand("S", new SelectionCommand(engine, this.invoker, this.recorder));
+        invoker.execute("S");
+        invoker.addCommand("C", new CopyCommand(engine, this.invoker, this.recorder));
+        invoker.execute("C");
+        String clipboardTest = engine.getClipboardContents();
+        assertEquals("This", clipboardTest);
+    }
+    
+    
+    @Test
+    void testExecutingDeleteCommand() {
+        invoker.setTextToBeInserted(TEST_STRING);
+        invoker.addCommand("I", new InsertCommand(engine, this.invoker, this.recorder));
+        invoker.execute("I");
+        invoker.setBeginIndex(0);
+        invoker.setEndIndex(4);
+        invoker.addCommand("S", new SelectionCommand(engine, this.invoker, this.recorder));
+        invoker.execute("S");
+        invoker.addCommand("D", new DeleteCommand(engine, this.invoker, this.recorder));
+        invoker.execute("D");
+        assertEquals(" is a test string that will be asserted", engine.getBufferContents());
+        Selection selectionTest = engine.getSelection();
+        assertEquals(0,selectionTest.getBeginIndex());
+        assertEquals(0,selectionTest.getEndIndex());
+        
+        invoker.setBeginIndex(6);
+        invoker.setEndIndex(10);
+        invoker.addCommand("S", new SelectionCommand(engine, this.invoker, this.recorder));
+        invoker.execute("S");
+        invoker.addCommand("D", new DeleteCommand(engine, this.invoker, this.recorder));
+        invoker.execute("D");
+        assertEquals(" is a  string that will be asserted", engine.getBufferContents());
+        selectionTest = engine.getSelection();
+        assertEquals(6,selectionTest.getBeginIndex());
+        assertEquals(6,selectionTest.getEndIndex());
+
+    }
+    
+    @Test
+    void testExecutingCutCommand() {
+        invoker.setTextToBeInserted(TEST_STRING);
+        invoker.addCommand("I", new InsertCommand(engine, this.invoker, this.recorder));
+        invoker.execute("I");
+        invoker.setBeginIndex(0);
+        invoker.setEndIndex(4);
+        invoker.addCommand("S", new SelectionCommand(engine, this.invoker, this.recorder));
+        invoker.execute("S");
+        invoker.addCommand("CU", new CutCommand(engine, this.invoker, this.recorder));
+        invoker.execute("CU");
+        String clipboardTest = engine.getClipboardContents();
+        assertEquals("This", clipboardTest);
+        assertEquals(" is a test string that will be asserted", engine.getBufferContents());
+        Selection selectionTest = engine.getSelection();
+        assertEquals(0,selectionTest.getBeginIndex());
+        assertEquals(0,selectionTest.getEndIndex());
+    }
+    
+    
+    @Test
+    void testExecutingPasteCommand() {
+        invoker.setTextToBeInserted(TEST_STRING);
+        invoker.addCommand("I", new InsertCommand(engine, this.invoker, this.recorder));
+        invoker.execute("I");
+        invoker.setBeginIndex(0);
+        invoker.setEndIndex(4);
+        invoker.addCommand("S", new SelectionCommand(engine, this.invoker, this.recorder));
+        invoker.execute("S");
+        invoker.addCommand("C", new CopyCommand(engine, this.invoker, this.recorder));
+        invoker.execute("C");
+        invoker.setBeginIndex(engine.getSelection().getBufferEndIndex());
+        invoker.setEndIndex(engine.getSelection().getBufferEndIndex());
+        invoker.addCommand("S", new SelectionCommand(engine, this.invoker, this.recorder));
+        invoker.execute("S");
+        invoker.addCommand("P", new PasteCommand(engine, this.invoker, this.recorder));
+        invoker.execute("P");
+        assertEquals("This is a test string that will be assertedThis",engine.getBufferContents());
+    }
+    
+    @Test 
+    void testExecutingReplayInsertCommand() {
+        invoker.setTextToBeInserted(TEST_STRING);
+        invoker.addCommand("I", new InsertCommand(engine, this.invoker, this.recorder));
+        invoker.execute("I");
+        invoker.setBeginIndex(0);
+        invoker.setEndIndex(4);
+        invoker.addCommand("S", new SelectionCommand(engine, this.invoker, this.recorder));
+        invoker.execute("S");
+        invoker.addCommand("R", new Replay(engine, this.invoker, this.recorder));
+        invoker.execute("R");
+        Selection selection = engine.getSelection();
+        assertEquals(selection.getBeginIndex(),0);     
+        assertEquals(selection.getEndIndex(),4);     
+              
+        //test replay with selection - what is even supposed to happen?
+    }
+    
+    @Test 
+    void testExecutingReplaySelectionCommand() {
+        invoker.setTextToBeInserted(TEST_STRING);
+        invoker.addCommand("I", new InsertCommand(engine, this.invoker, this.recorder));
+        invoker.execute("I");
+        invoker.addCommand("R", new Replay(engine, this.invoker, this.recorder));
+        invoker.execute("R");
+        assertEquals("This is a test string that will be assertedThis is a test string that will be asserted",engine.getBufferContents());     
+               
+    }
+    
+    //test replay with no memento commands 
+
+
+    //fix cut with no selection and delete at the beginning of the buffer
+    
 }
