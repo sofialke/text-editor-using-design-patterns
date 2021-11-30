6c59b9ae4a91be9705f11688c3ea43d99a5d6ee5
Marta <marta.moreno@etudiant.univ-rennes1.fr>
2021-11-30 19:25:43 +0100

    commented commands

--- src/TextEditor/CutCommand.java
+++ src/TextEditor/CutCommand.java
@@ -3,30 +3,48 @@ package TextEditor;
 import java.util.Optional;
 
 /**
- * Class that implements paste command by implementing the Command interface.
+ * Class that implements cut command by implementing the Recordable interface.
  */
 
 public class CutCommand implements Recordable{
     Engine engine;
     Recorder recorder;
 
+    /**
+     * CutCommand constructor that initializes a cut command instance
+     * @param engine
+     * @param invoker
+     * @param recorder
+     */
     public CutCommand(Engine engine, Invoker invoker, Recorder recorder){
         this.engine = engine;
         Optional<Memento> memento = Optional.empty();
         this.recorder = recorder;
     }
-
-    public Optional<Memento> getMemento(){
-        return Optional.empty();
-    }
-
-    public void setMemento(Memento memento){
-
-    }
-
+    
+    /**
+     * Executes the cut action in the engine and saves the command in the recorder
+     */
     public void execute(){
         engine.cutSelectedText();
         recorder.save(this);
     }
 
+
+    /**
+     * Provides empty Optional memento object
+     * 
+     * @return empty Optional object
+     */
+    public Optional<Memento> getMemento(){
+        return Optional.empty();
+    }
+
+    /**
+     * Implements the setMemento operation without any action since the copy command generates no memento
+     */
+    public void setMemento(Memento memento){
+
+    }
+    
 }
