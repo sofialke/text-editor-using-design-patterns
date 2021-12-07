147219ec970149c53132e3fdab3df68f03b89e27
Zosia Walczewska <zosia.walczewska@gmail.com>
2021-12-07 15:50:06 +0100

    implementation of functions added to undomanager. Comments added to UndoMAnager.

--- src/TextEditor/UndoManager.java
+++ src/TextEditor/UndoManager.java
@@ -3,24 +3,51 @@ package TextEditor;
 import java.util.List;
 import java.util.Objects;
 
+/**
+ * Caretaker class for Engine. It saves the past and future states of the engine, as well as performs redo and undo
+ * on the engine.
+ */
 public class UndoManager {
 
     private List<Memento> pastStates;
     private List<Memento> futureStates;
     private Engine engine;
 
+    /**
+     * Constructor for the UndoManager with engine as a parameter
+     * @param engine- EngineImpl instance.
+     */
+    public UndoManager(Engine engine){
+        this.engine = engine;
+    }
+
+    /**
+     * Method that stores the current memento state in the list.
+     */
     public void store(){
-        //memento = engine.getMemento
-        //put memento in paststates
+        Memento memento = this.engine.getMemento();
+        this.pastStates.add(memento);
     }
 
+    /**
+     * Method that undo's the last change in the engine.
+     */
     public void undo(){
-        //setMemento(last memento from the pastlist)
-        //last state goes to future states
+        Memento currentMemento = this.engine.getMemento();
+        Memento pastMemento = pastStates.get(pastStates.size()-1);
+        this.futureStates.add(currentMemento);
+        this.engine.setMemento(pastMemento);
+        this.pastStates.remove(pastMemento);
     }
 
+    /**
+     * Method that redo's the last change in the engine.
+     */
     public void redo(){
-        //setMemento(last memento from the futurelist)
-        //
+        Memento currentMemento = this.engine.getMemento();
+        Memento futureMemento = this.futureStates.get(futureStates.size()-1);
+        this.engine.setMemento(futureMemento);
+        this.pastStates.add(currentMemento);
+        this.futureStates.remove(futureMemento);
     }
 }
